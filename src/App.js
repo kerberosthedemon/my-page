917c3682808fba85d5898010fd9a91e74120943a
kerberosthedemon <kerberosthedemon@yandex.com>
2020-08-16 12:39:44 -0300

    added loading screen

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './pages/home/Home'
 
 export default function App() {
   return (
-    <Home></Home>
+    <Home />
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './home.css';
+import { useProgressiveImage } from './../../shared/useProgressiveImage';
+import backgroundPic from './../../img/adeole_yosemite.jpg';
 
 export default function Home() {
+  const loaded = useProgressiveImage(`${backgroundPic}`);
+
   return (
-    <section>
-      <div className="box">
-        <h2 style={{ textAlign: 'center' }}>Bienvenido</h2>
-        <p>Este es mi sito web, soy un desarrollador de tiempo completo, me entusiasma mucho la programacion
-        <br />y deseo algun dia poder lograr mis sueños a traves de mis programas
-        <br />Aqui dejo unos links para que puedas ver mis trabajos, muchas gracias por visitar mi sitio web!</p>
-      </div>
-    </section>
+    <React.Fragment>
+      {!loaded && <div>Loading...</div>}
+      {loaded && <section>
+        <div className="box">
+          <h2>Bienvenido</h2>
+          <p>Este es mi sito web, soy un desarrollador de tiempo completo, me entusiasma mucho la programacion
+          <br />y deseo algun dia poder lograr mis sueños a traves de mis programas
+          <br />Aqui dejo unos links para que puedas ver mis trabajos, muchas gracias por visitar mi sitio web!</p>
+        </div>
+      </section>}
+    </React.Fragment>
 
   );
 }
