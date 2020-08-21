0331144e7a34ff47a99ddf92f39f6a8d5b0bf5c0
kerberosthedemon <kerberosthedemon@yandex.com>
2020-08-21 16:13:51 -0300

    switched to grid layout on infoText component

--- src/pages/home/InfoText/IconCard/IconCard.jsx
+++ src/pages/home/InfoText/IconCard/IconCard.jsx
@@ -3,15 +3,13 @@ import { Grid, Card, CardContent, Typography } from '@material-ui/core';
 
 export default function IconCard({ text, children }) {
   return (
-    <Grid item xs={4}>
-      <Card variant="outlined">
-        <CardContent>
-          <Grid container direction="column" alignContent="center" justify="center" alignItems="center">
-            <Grid item>{children}</Grid>
-            <Grid item><Typography>{text}</Typography></Grid>
-          </Grid>
-        </CardContent>
-      </Card>
-    </Grid>
+    <Card variant="outlined">
+      <CardContent>
+        <Grid container direction="column" alignContent="center" justify="center" alignItems="center">
+          <Grid item>{children}</Grid>
+          <Grid item><Typography>{text}</Typography></Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   )
 }
--- src/pages/home/InfoText/InfoText.jsx
+++ src/pages/home/InfoText/InfoText.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core';
-import { Grid } from '@material-ui/core';
 import { Card } from '@material-ui/core';
 import { CardContent } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
@@ -24,21 +23,52 @@ const useStyles = makeStyles(theme => {
       // width: '100%',
       // maxWidth: '600px',
     },
-    paper: {
-      height: 'calc(268px + 200px + 75px)',
-      // transform: 'translateY(37px)',
-      borderRadius: 0
-    },
-    cardContent: {
-      height: '100%'
-    },
     textContainer: {
       maxHeight: '200px',
       overflow: 'auto'
     },
-    cardHeader: {
+    gridContainer: {
+      display: 'grid',
+      'grid-template-rows': '65px 1fr 2fr 1fr',
+      'grid-template-columns': 'repeat(3, 1fr)',
+      height: '545px',
+      maxWidth: '600px',
+      borderRadius: 0,
+      transform: 'translateY(36px)',
+      [theme.breakpoints.down('sm')]: {
+        width: '300.6px !important',
+        transform: 'unset'
+      },
+    },
+    gridTitle: {
+      gridColumn: '1/4',
       backgroundColor: theme.palette.secondary.main,
-      color: 'white'
+      color: 'white',
+      padding: theme.spacing(2),
+    },
+    gridText: {
+      gridColumn: '1/4',
+      padding: theme.spacing(2)
+    },
+    gridPicture: {
+      gridColumn: '1/4',
+      backgroundImage: 'url(https://safebooru.org//images/3050/6b7b67fe97714483c544bbf4bd6cd9d90875ba54.jpg?3176055)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center'
+    },
+    gridIcon: {
+      gridColumn: '1fr',
+      alignSelf: 'center',
+      justifySelf: 'center'
+    },
+    gridIcon1: {
+      paddingLeft: theme.spacing(2),
+    },
+    gridIcon2: {
+      paddingLeft: theme.spacing(2),
+    },
+    gridIcon3: {
+      paddingLeft: theme.spacing(2),
     }
   }
 });
@@ -47,38 +77,30 @@ export default function InfoText() {
   const classes = useStyles();
 
   return (
-    <Grid container direction="column" justify="center" className={classes.container}>
-      <Grid item className={classes.item}>
-        <Card className={classes.paper}>
-          <CardContent className={classes.cardHeader}>
-            <Typography variant="h5">
-              Bienvenido
+    <Card className={classes.gridContainer}>
+      <div className={classes.gridTitle}>
+        <Typography variant="h5">
+          Bienvenido
+        </Typography>
+      </div>
+
+      <div className={classes.gridText}>
+        <Card variant="outlined">
+          <CardContent className={classes.textContainer}>
+            <Typography variant="body2">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque eveniet non officia cupiditate rerum explicabo.<br />
+                    Magnam quam doloremque eos obcaecati blanditiis saepe ducimus rerum quas, maiores voluptates non dolores possimus?
             </Typography>
           </CardContent>
-          <CardContent>
-            <Grid container spacing={1} direction="row" className={classes.cardContent}>
-
-              <Grid item>
-                <Card variant="outlined">
-                  <CardContent className={classes.textContainer}>
-                    <Typography variant="body2">
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque eveniet non officia cupiditate rerum explicabo.<br />
-                      Magnam quam doloremque eos obcaecati blanditiis saepe ducimus rerum quas, maiores voluptates non dolores possimus?
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item container xs={12}>
-                <Grid container spacing={1} justify="space-between">
-                  <IconCard text="Code"><CodeIcon fontSize="large" /></IconCard>
-                  <IconCard text="Web"><LanguageOutlinedIcon fontSize="large" /></IconCard>
-                  <IconCard text="Games"><SportsEsportsOutlinedIcon fontSize="large" /></IconCard>
-                </Grid>
-              </Grid>
-            </Grid>
-          </CardContent>
         </Card>
-      </Grid>
-    </Grid>
+      </div>
+
+      <div className={classes.gridPicture}></div>
+
+      <div className={`${classes.gridIcon} ${classes.icon1}`}><IconCard text="Code"><CodeIcon fontSize="large" /></IconCard></div>
+      <div className={`${classes.gridIcon} ${classes.icon2}`}><IconCard text="Web"><LanguageOutlinedIcon fontSize="large" /></IconCard></div>
+      <div className={`${classes.gridIcon} ${classes.icon3}`}><IconCard text="Games"><SportsEsportsOutlinedIcon fontSize="large" /></IconCard></div>
+
+    </Card>
   )
 }
